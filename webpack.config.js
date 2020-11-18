59ec3556c51726836b8345ed3f7155359ac58504
Jordi Galobart <github@galu.cat>
2020-11-18 09:49:33 +0100

    source init

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const path = require("path");
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src/js','index.js'),
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+       },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+	            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+	            type: 'asset/resource',
+		    }	
+        ],
+    },
+    optimization: {
+        splitChunks: {
+          chunks: 'all',
+        },
+      },
+    plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html")
+    }),
+    new CleanWebpackPlugin(),
+    ]
+};
